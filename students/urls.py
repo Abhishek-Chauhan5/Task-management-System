72c69b298afaf14c79d5afc7b3031eea30493485
Abhishek-Chauhan5 <abhishek630646@gmail.com>
2026-09-16 16:11:56 +0530

    Add home route

--- students/urls.py
+++ students/urls.py
@@ -16,8 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
+
+
+def home(request):
+    return redirect('/student/web/login/')
+
 
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
     path('student/', include('student.urls')),
 ]
